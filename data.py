d20e1c20d164bc1bf6b4fd4df4a20a5e26582eb2
Samarth <sschintak7@gmail.com>
2023-02-12 17:36:49 +0530

    New modules added to run the the program easily on any system without installing request module

--- data.py
+++ data.py
@@ -1,12 +1,40 @@
-import requests
+
+import bs4
+import requests as rq
+from sys import platform
+import unicodedata
+import time
+import pip
+import os
+
+
+def install(package):
+    if hasattr(pip, 'main'):
+        pip.main(['install', package])
+    else:
+        pip._internal.main(['install', package])
+
+
+while True:
+
+    try:
+        modules = ['bs4', 'requests']
+        for module in modules:
+            install(module)
+        break
+    except:
+        print('Bad Internet Connection')
+        time.sleep(5)
+
 
 parameters = {
     "amount": 10,
-    "category":18,
+    "category": 18,
     "type": "boolean",
 }
 
-response = requests.get("https://opentdb.com/api.php", params=parameters)
+response = rq.get("https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 data = response.json()
 question_data = data["results"]
+print(question_data)
